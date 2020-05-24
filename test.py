a2f4503d0a47a3f32e9d26806861d5a39eb3ee78
Skyrich <petercha2000@gmail.com>
2020-05-24 18:16:29 +0900

    보안키 추가

--- test.py
+++ test.py
@@ -8,34 +8,27 @@ import random
 im = Image.open('screenshot1.png').crop((240, 365, 679, 484))
 im = im.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(10)
-#im = im.convert('1')
+im = enhancer.enhance(3)
+im = im.convert('1')
 im.save('temp2.png')
 
-token = "!.,'-_^*1234567890+="
-def addtoken(st):
-    out = ""
-    for t in st:
-        out += t
-        if random.choice([True, False]):
-            out += token[random.randint(0, len(token)-1)]
-    return out
-print(addtoken("좆까 씨발새끼야 이미 갤 좆창났어 그냥 꺼져"))
 
-kernel = np.ones((5,5), np.uint8)
-im_gray = cv2.imread('temp2.png', cv2.IMREAD_GRAYSCALE)
-im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_CLOSE, kernel)
+kernel1 = np.ones((5, 5), np.uint8)
+kernel2 = np.ones((4, 4), np.uint8)
+
+im_gray = cv2.imread('temp2.png')#, cv2.IMREAD_GRAYSCALE)
+height, width = im_gray.shape[:2]
+#im_gray = cv2.resize(im_gray, (int(1.3*width), 2*height), interpolation=cv2.INTER_LINEAR)
 #im_gray = cv2.fastNlMeansDenoising(im_gray, None, 3, 7, 21)
-#im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_OPEN, kernel)
+im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_CLOSE, kernel1)
+im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_OPEN, kernel2)
 (thresh, im_gray) = cv2.threshold(im_gray, 110, 255, cv2.THRESH_BINARY_INV )
-im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_OPEN, kernel)
 cv2.imwrite("tt.png", im_gray)
 _text = pytesseract.image_to_string(im_gray, lang='eng', config='-psm 8 -oem 3 -l eng')
-
 #text = pytesseract.image_to_string(Image.open('temp2.png'))
 text = ""
 for t in _text:
     if t.isalpha() or t.isdigit():
         text += t
 
-print(":", _text, " :", text)
+print(":", _text, " :"+text)
--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.proxy import Proxy, ProxyType
-#import pytesseract
-#from PIL import Image, ImageEnhance, ImageFilter
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 import cv2
@@ -9,9 +9,9 @@ import numpy as np
 import random
 
 token = "!.,'-_^*1234567890+="
-title = ["싱글벙글 글쓰기 힘드네"]
-content = ["좆같다"]
-flag = False
+title = ["싱글벙글 노잼"]
+content = ["에휴"]
+flag = 0
 
 def addtoken(st):
     out = ""
@@ -24,6 +24,9 @@ def addtoken(st):
 def wait():
     time.sleep(random.randint(2,6)/2)
 
+def isr(t):
+    return t in "qwertyuiopasdfghjklzxcvbnm0123456789"
+
 options = webdriver.ChromeOptions()
 options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
 options.add_argument('--headless')
@@ -34,41 +37,109 @@ options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", chrome_options=options)
 
 while(True):
-    print("시작")
-    driver.get('http://icanhazip.com/')
-    print("::::IP : ", driver.find_element_by_xpath('/html/body/pre').get_attribute('innerHTML'))
+    if flag == 0:
+        print("시작")
+        driver.get('http://icanhazip.com/')
+        print("::::IP : ", driver.find_element_by_xpath('/html/body/pre').get_attribute('innerHTML'))
 
-    driver.get('https://gall.dcinside.com/mgallery/board/write/?id=singlebungle1472')
-    driver.implicitly_wait(20)
-    print("화면 뜸")
-    wait()
+        driver.get('https://gall.dcinside.com/mgallery/board/write/?id=singlebungle1472')
+        driver.implicitly_wait(20)
+        print("화면 뜸")
+        wait()
 
-    driver.find_element_by_xpath('//*[@id="name"]').send_keys("ㅇㅇ")
-    print("닉네임 입력")
-    wait()
+        try:
+            driver.find_element_by_xpath('//*[@id="name"]').send_keys("ㅇㅇ")
+            print("닉네임 입력")
+            wait()
+        except:
+            print("닉네임 입력 실패 - 재시작")
+            flag = 0
+            continue
 
-    driver.find_element_by_xpath('//*[@id="password"]').send_keys("85749869")
-    print("비번 입력")
-    wait()
+        try:
+            driver.find_element_by_xpath('//*[@id="password"]').send_keys("85749869")
+            print("비번 입력")
+            wait()
+        except:
+            print("비번 입력 실패 - 재시작")
+            flag = 0
+            continue
 
-    #driver.find_element_by_xpath('//*[@id="write"]/div[1]/fieldset/div[3]/ul/li').click()
-    #print("말머리 선택")
-    #wait()
+        #driver.find_element_by_xpath('//*[@id="write"]/div[1]/fieldset/div[3]/ul/li').click()
+        #print("말머리 선택")
+        #wait()
+        try:
+            driver.find_element_by_xpath('//*[@id="subject"]').send_keys("싱글벙글 "+random.choice(title))
+            print("제목 입력")
+            wait()
+        except:
+            print("제목 입력 실패 - 재시작")
+            flag = 0
+            continue
 
-    driver.find_element_by_xpath('//*[@id="subject"]').send_keys("싱글벙글 "+random.choice(title))
-    print("제목 입력")
-    wait()
-
-    while(True):
+    if flag == 0 or flag == 2:
         body = driver.switch_to_frame(driver.find_element_by_xpath('//*[@id="tx_canvas_wysiwyg"]'))
         driver.find_element_by_xpath('/html/body').send_keys(random.choice(content))
         driver.switch_to_default_content()
         print("본문 입력")
         wait()
 
-        driver.find_element_by_xpath("//button[@class='btn_blue btn_svc write']").click()
-        print("제출 클릭")
+    if flag == 3:
+        while(True):
+            driver.execute_script("document.body.style.zoom='350%'")
+            try:
+                target = driver.find_element_by_xpath('//*[@id="kcaptcha"]')
+            except:
+                flag = 0
+                break
+            actions = ActionChains(driver)
+            actions.move_to_element(target)
+            actions.perform()
+            driver.execute_script("window.scrollBy(100, 100)")
+            driver.save_screenshot("screenshot1.png")
+            print("스샷 찍음")
+
+            im = Image.open('screenshot1.png').crop((240, 365, 679, 484))
+            im = im.filter(ImageFilter.MedianFilter())
+            enhancer = ImageEnhance.Contrast(im)
+            im = enhancer.enhance(10)
+            im.save('temp2.png')
+
+            kernel = np.ones((5, 5), np.uint8)
+            im_gray = cv2.imread('temp2.png', cv2.IMREAD_GRAYSCALE)
+            im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_CLOSE, kernel)
+            (thresh, im_gray) = cv2.threshold(im_gray, 110, 255, cv2.THRESH_BINARY_INV)
+            im_gray = cv2.morphologyEx(im_gray, cv2.MORPH_OPEN, kernel)
+            _text = pytesseract.image_to_string(im_gray, lang='eng', config='-psm 8 -oem 3 -l eng')
+
+            text = ""
+            for t in _text:
+                if isr(t):
+                    text += t
+
+            print(":", _text, " :"+text)
+            if len(text) == 8:
+                break
+
+            print("틀려서 클릭후 기다림")
+            driver.execute_script("document.body.style.zoom='100%'")
+            target.click()
+            time.sleep(4)
+
+    if flag == 3:
+        try:
+            driver.execute_script("document.body.style.zoom='100%'")
+            driver.find_element_by_xpath('//*[@id="code"]').send_keys(text)
+            wait()
+        except:
+            flag = 0
+            pass
+
+
+    driver.find_element_by_xpath("//button[@class='btn_blue btn_svc write']").click()
+    print("제출 클릭")
         
+    while(True):
         print("알람 뜰때까지 7초 기다림")
         time.sleep(7)
         try:
@@ -76,10 +147,24 @@ while(True):
             alert = driver.switch_to.alert
             print(alert.text)
             if "차단" in alert.text:
-                print("IP 바꿔야함")
-                flag = True
+                print("차단 당해서 IP 바꿔야함")
+                flag = 1
+            if "올바른" in alert.text:
+                print("기계 인거 들킴")
+                flag = 0
+            if "내용" in alert.text:
+                print("본문 제대로 안씀")
+                flag = 2
+            if "코드" in alert.text or "code" in alert.text:
+                print("코드 틀림")
+                flag = 3
             alert.accept()
-            print("본문 내용 다시 입력")
+            
         except:
-            print("알림 안뜸 - 제출 성공?")
+            print("알림 안뜸 - 제출 성공 or 오류")
+            flag = 0
             break
+    
+    if flag == 1:
+        print("시스템 종료")
+        pass
