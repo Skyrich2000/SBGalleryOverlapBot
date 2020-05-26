4cc9f9ca96e76125a1eb97d53e4d0cf970ed3f2b
Skyrich <petercha2000@gmail.com>
2020-05-26 18:37:52 +0900

    유저 에이전트 변경

--- test2.py
+++ test2.py
@@ -9,13 +9,13 @@ import time
 import cv2
 import os
 
-token = "!.,'-_^*1234567890+="
 title = ["노잼", "망함"]
-content = ["작작해"]
+content = ["에휴"]
 flag = 0
 retry = 0
 
 def addtoken(st):
+    token = "!.,'-_^*1234567890+="
     out = ""
     for t in st:
         out += t
@@ -37,13 +37,13 @@ options = webdriver.ChromeOptions()
 options.add_argument("\"accept\"=*/*")
 options.add_argument("accept-encoding=\"sdch\"")
 options.add_argument("accept-language=ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4")
-options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
 options.add_argument('window-size=800x600')
 options.add_argument('--headless')
 #options.add_argument('--no-sandbox')
 #options.add_argument('--disable-dev-shm-usage')
 #options.add_argument("--proxy-server=socks5://127.0.0.1:9150")
-#options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
+options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
 
 #driver = webdriver.Chrome(chrome_options=options)
 driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver",chrome_options=options)
@@ -51,7 +51,7 @@ print("시작")
 driver.get('http://icanhazip.com/')
 print("::::IP : ", driver.find_element_by_xpath('/html/body/pre').get_attribute('innerHTML'))
 
-driver.get('https://gall.dcinside.com/mgallery/board/lists?id=singlebungle1472')
+driver.get('https://gall.dcinside.com/mgallery/board/lists?id=singlebungle1472&page=1')
 wait()
 
 try:
